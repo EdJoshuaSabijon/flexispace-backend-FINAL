f0530817aee10f7c034dec47255e9421a1b1f987
Ed Joshua Sabijon <ed.sabijon@urios.edu.ph>
2026-05-05 01:44:57 +0800

    feat: add image_url accessor to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,6 +21,21 @@ class Product extends Model
         'is_active' => 'boolean',
     ];
 
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute()
+    {
+        if (!$this->image_path) {
+            return 'https://via.placeholder.com/150?text=No+Image';
+        }
+        
+        if (filter_var($this->image_path, FILTER_VALIDATE_URL)) {
+            return $this->image_path;
+        }
+
+        return asset('storage/' . $this->image_path);
+    }
+
     public function orderItems()
     {
         return $this->hasMany(OrderItem::class);
